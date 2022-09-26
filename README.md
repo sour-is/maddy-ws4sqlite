483bc51830909554703ba8c438b20ce1fd29bd6a
Jon Lundy <jon@xuu.cc>
2022-09-26 16:21:25 -0600

    feat: add refresh cache

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ ws4sqlite interfaces for maddy.email
 table.ws4sql_query aliases {
     url    http://example.com/db1
     lookup "SELECT alias FROM aliases WHERE email = :key"
-    list   "SELECT alias FROM aliases"
+    list   "SELECT email FROM aliases"
     add    "INSERT INTO aliases (alias, email) VALUES (:key, :value)"
     set    "UPDATE aliases SET email = :value WHERE alias = :key"
     del    "DELETE FROM aliases WHERE alias = :key "
+}
+
+table.ws4sql_cache aliases {
+    ... same as above ...
+
+    cache_size 1000
 }
 ```
--- table/table.go
+++ table/table.go
@@ -3,41 +3,45 @@ package table
 import (
 	"context"
 	"fmt"
+	"io"
+	"time"
 
 	"github.com/foxcpp/maddy/framework/config"
 	"github.com/foxcpp/maddy/framework/module"
+	lru "github.com/hashicorp/golang-lru"
 
-	ws4 "github.com/proofrock/ws4sqlite-client-go"
+	ws4 "github.com/jonlundy/ws4sqlite-client-go"
 )
 
-type SQL struct {
+type Table struct {
 	modName  string
 	instName string
 
-	db     *ws4.Client
 	lookup string
 	add    string
 	list   string
 	set    string
 	del    string
+
+	sql querier
 }
 
-func NewSQL(modName, instName string, _, _ []string) (module.Module, error) {
-	return &SQL{
+func NewTable(modName, instName string, _, _ []string) (module.Module, error) {
+	return &Table{
 		modName:  modName,
 		instName: instName,
 	}, nil
 }
 
-func (s *SQL) Name() string {
+func (s *Table) Name() string {
 	return s.modName
 }
 
-func (s *SQL) InstanceName() string {
+func (s *Table) InstanceName() string {
 	return s.instName
 }
 
-func (s *SQL) Init(cfg *config.Map) error {
+func (s *Table) Init(cfg *config.Map) error {
 	var (
 		initQueries []string
 		url         string
@@ -50,6 +54,10 @@ func (s *SQL) Init(cfg *config.Map) error {
 	cfg.String("list", false, false, "", &s.list)
 	cfg.String("del", false, false, "", &s.del)
 	cfg.String("set", false, false, "", &s.set)
+
+	var cacheSize int64
+	cfg.Int64("cache_size", false, false, 10_000, &cacheSize)
+
 	if _, err := cfg.Process(); err != nil {
 		return err
 	}
@@ -58,7 +66,15 @@ func (s *SQL) Init(cfg *config.Map) error {
 	if err != nil {
 		return config.NodeErr(cfg.Block, "failed to open db: %v", err)
 	}
-	s.db = db
+	s.sql = &wsql{db}
+
+	if cacheSize > 0 {
+		c, err := lru.New(int(cacheSize))
+		if err != nil {
+			return err
+		}
+		s.sql = &cache{sql: s.sql, c: c}
+	}
 
 	if len(initQueries) > 0 {
 
@@ -79,93 +95,48 @@ func (s *SQL) Init(cfg *config.Map) error {
 	return nil
 }
 
-func (s *SQL) Close() error {
-	return nil
+func (s *Table) Close() error {
+	return s.sql.Close()
 }
 
-func (s *SQL) Lookup(ctx context.Context, val string) (string, bool, error) {
-	var (
-		repl string
-	)
+func (s *Table) Lookup(ctx context.Context, val string) (string, bool, error) {
 	req, err := ws4.NewRequestBuilder().AddQuery(s.lookup).WithValues(map[string]interface{}{"key": val}).Build()
 	if err != nil {
 		return "", false, fmt.Errorf("%s: lookup %s: %w", s.modName, val, err)
 	}
-	res, _, err := s.db.Send(req)
+	lis, err := s.sql.query(ctx, "L"+val, req)
 	if err != nil {
-		return "", false, fmt.Errorf("%s: lookup %s: %w", s.modName, val, err)
+		err = fmt.Errorf("%s: lookup %s: %w", s.modName, val, err)
 	}
-	for _, r := range res.Results {
-		if !r.Success {
-			return "", false, fmt.Errorf("%s: lookup %s: %s", s.modName, val, r.Error)
-		}
-		for _, s := range r.ResultSet {
-			for _, v := range s {
-				repl = fmt.Sprint(v)
-				return repl, true, nil
-			}
-		}
+
+	if len(lis) == 0 {
+		return "", false, err
 	}
-	return repl, false, nil
+	return lis[0], true, err
 }
 
-func (s *SQL) LookupMulti(ctx context.Context, val string) ([]string, error) {
-	var (
-		repl []string
-	)
+func (s *Table) LookupMulti(ctx context.Context, val string) ([]string, error) {
 	req, err := ws4.NewRequestBuilder().AddQuery(s.lookup).WithValues(map[string]interface{}{"key": val}).Build()
 	if err != nil {
-		return repl, fmt.Errorf("%s: lookup-multi %s: %w", s.modName, val, err)
+		return nil, fmt.Errorf("%s: lookup-multi %s: %w", s.modName, val, err)
 	}
-	res, _, err := s.db.Send(req)
-	if err != nil {
-		return repl, fmt.Errorf("%s: lookup-multi %s: %w", s.modName, val, err)
-	}
-	for _, r := range res.Results {
-		if !r.Success {
-			return repl, fmt.Errorf("%s: lookup-multi %s: %v", s.modName, val, r.Error)
-		}
-		for _, s := range r.ResultSet {
-			for _, v := range s {
-				repl = append(repl, fmt.Sprint(v))
-				break
-			}
-		}
-	}
-	return repl, nil
+	lis, err := s.sql.query(ctx, "M"+val, req)
+	return lis, fmt.Errorf("%s: lookup-multi: %w", s.modName, err)
 }
 
-func (s *SQL) Keys() ([]string, error) {
+func (s *Table) Keys() ([]string, error) {
 	if s.list == "" {
 		return nil, fmt.Errorf("%s: table is not mutable (no 'list' query)", s.modName)
 	}
-
-	var (
-		repl []string
-	)
-	req, err := ws4.NewRequestBuilder().AddQuery(s.lookup).Build()
+	req, err := ws4.NewRequestBuilder().AddQuery(s.list).Build()
 	if err != nil {
-		return repl, fmt.Errorf("%s: list: %w", s.modName, err)
+		return nil, fmt.Errorf("%s: list: %w", s.modName, err)
 	}
-	res, _, err := s.db.Send(req)
-	if err != nil {
-		return repl, fmt.Errorf("%s: list: %w", s.modName, err)
-	}
-	for _, r := range res.Results {
-		if !r.Success {
-			return repl, fmt.Errorf("%s: list: %v", s.modName, r.Error)
-		}
-		for _, s := range r.ResultSet {
-			for _, v := range s {
-				repl = append(repl, fmt.Sprint(v))
-			}
-			return repl, nil
-		}
-	}
-	return repl, nil
+	lis, err := s.sql.query(context.TODO(), "K", req)
+	return lis, fmt.Errorf("%s: list: %w", s.modName, err)
 }
 
-func (s *SQL) RemoveKey(k string) error {
+func (s *Table) RemoveKey(k string) error {
 	if s.del == "" {
 		return fmt.Errorf("%s: table is not mutable (no 'del' query)", s.modName)
 	}
@@ -174,7 +145,7 @@ func (s *SQL) RemoveKey(k string) error {
 	if err != nil {
 		return fmt.Errorf("%s: del %s: %w", s.modName, k, err)
 	}
-	_, _, err = s.db.Send(req)
+	_, err = s.sql.exec(context.TODO(), req)
 	if err != nil {
 		return fmt.Errorf("%s: del %s: %w", s.modName, k, err)
 	}
@@ -184,7 +155,7 @@ func (s *SQL) RemoveKey(k string) error {
 	return nil
 }
 
-func (s *SQL) SetKey(k, v string) error {
+func (s *Table) SetKey(k, v string) error {
 	if s.set == "" {
 		return fmt.Errorf("%s: table is not mutable (no 'set' query)", s.modName)
 	}
@@ -196,7 +167,7 @@ func (s *SQL) SetKey(k, v string) error {
 	if err != nil {
 		return fmt.Errorf("%s: set %s: %w", s.modName, k, err)
 	}
-	res, _, err := s.db.Send(req)
+	res, err := s.sql.exec(context.TODO(), req)
 	if err != nil {
 		return fmt.Errorf("%s: set %s: %w", s.modName, k, err)
 	}
@@ -207,7 +178,7 @@ func (s *SQL) SetKey(k, v string) error {
 			if err != nil {
 				return fmt.Errorf("%s: add %s: %w", s.modName, k, err)
 			}
-			_, _, err = s.db.Send(req)
+			_, err = s.sql.exec(context.TODO(), req)
 			if err != nil {
 				return fmt.Errorf("%s: add %s: %w", s.modName, k, err)
 			}
@@ -218,6 +189,102 @@ func (s *SQL) SetKey(k, v string) error {
 	return nil
 }
 
+type querier interface {
+	io.Closer
+	query(context.Context, string, *ws4.Request) ([]string, error)
+	exec(context.Context, *ws4.Request) (*ws4.Response, error)
+}
+
+type wsql struct {
+	db *ws4.Client
+}
+
+func (s *wsql) Close() error {
+	return nil
+}
+
+func (s *wsql) query(ctx context.Context, key string, req *ws4.Request) ([]string, error) {
+	var (
+		repl []string
+	)
+	res, _, err := s.db.SendWithContext(ctx, req)
+	if err != nil {
+		return repl, err
+	}
+	for _, r := range res.Results {
+		if !r.Success {
+			return repl, err
+		}
+		for _, s := range r.ResultSet {
+			for _, v := range s {
+				repl = append(repl, fmt.Sprint(v))
+				break
+			}
+		}
+	}
+	return repl, nil
+}
+func (s *wsql) exec(ctx context.Context, req *ws4.Request) (*ws4.Response, error) {
+	res, _, err := s.db.SendWithContext(ctx, req)
+	return res, err
+}
+
+type cache struct {
+	sql querier
+
+	c *lru.Cache
+}
+
+func (s *cache) Close() error {
+	s.c.Purge()
+	return s.sql.Close()
+}
+
+func (s *cache) exec(ctx context.Context, req *ws4.Request) (*ws4.Response, error) {
+	return s.sql.exec(ctx, req)
+}
+func (s *cache) query(ctx context.Context, key string, req *ws4.Request) ([]string, error) {
+	if s.c == nil {
+		return s.sql.query(ctx, key, req)
+	}
+
+	type cacheKey string
+	type cacheValue struct {
+		lis       []string
+		lastValid time.Time
+	}
+
+	refresh := func(ctx context.Context, cancel func()) (*cacheValue, error){
+		if cancel != nil {
+			defer cancel()
+		}
+
+		var err error
+		v := &cacheValue{lastValid: time.Now()}
+		v.lis, err = s.sql.query(ctx, key, req)
+		if err != nil {
+			s.c.Add(key, v)
+		}
+		return v, err
+	}
+
+	if v, ok := s.c.Get(cacheKey(key)); ok {
+		if lis, ok := v.(cacheValue); ok {
+			if time.Now().After(lis.lastValid.Add(30 * time.Minute)) {
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				go refresh(ctx, cancel)
+			}
+
+			return lis.lis, nil
+		}
+
+		return nil, nil
+	}
+
+	v, err := refresh(ctx, nil)
+	return v.lis, err
+}
+
 func init() {
-	module.Register("table.ws4sql_query", NewSQL)
+	module.Register("table.ws4sql_query", NewTable)
 }
